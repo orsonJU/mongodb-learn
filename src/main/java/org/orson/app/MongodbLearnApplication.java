9c99e2f54499e86ba391d0f95ec528bc32bb86be
orson.s.h.zhu <orson.s.h.zhu@outlook.com>
2019-01-02 19:13:08 +0800

    init

---
+++ src/main/java/org/orson/app/MongodbLearnApplication.java
@@ -0,0 +1,14 @@
+package org.orson.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongodbLearnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongodbLearnApplication.class, args);
+	}
+
+}
+
